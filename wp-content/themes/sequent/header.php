5ba7c2e6ab31ad457f35e4f1ad22d5ec27864098
Scott Revell <srevell@whitepenny.com>
2018-11-06 12:51:28 -0500

    Added GA Code

--- wp-content/themes/sequent/header.php
+++ wp-content/themes/sequent/header.php
@@ -15,6 +15,17 @@ $scripts_body = get_field( 'scripts_body', 'option' );
     <meta name="mobile-web-app-capable" content="yes">
 
     <link rel="stylesheet" href="https://use.typekit.net/puq8glt.css">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-53107265-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-53107265-1');
+    </script>
+
     
     <?php wp_head(); ?>
     <?php sl_favicons(); ?>
