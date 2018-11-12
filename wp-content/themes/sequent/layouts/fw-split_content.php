d465aa5cc2576184d4d5728a06c2fb10f5503bf0
Ben Plum <mr@benplum.com>
2018-11-12 14:37:35 -0500

    Switching order of split content sidebar callout

--- wp-content/themes/sequent/layouts/fw-split_content.php
+++ wp-content/themes/sequent/layouts/fw-split_content.php
@@ -6,6 +6,15 @@ $content = get_sub_field( 'content' );
 ?>
 <div class="split_content gravity_<?php echo $gravity; ?> section_padded bg_white" data-checkpoint-animation="fade-up">
   <div class="fs-row padded_row split_content_row">
+    <?php
+      if ( have_rows( 'sidebar' ) ) :
+        while ( have_rows( 'sidebar' ) ) :
+          the_row();
+
+          get_template_part( 'layouts/fw-split_content_sidebar', get_row_layout() );
+        endwhile;
+      endif;
+    ?>
     <div class="fs-cell fs-lg-8 page_container split_content_container split_content_cell">
       <?php if ( ! empty( $label ) ) : ?>
       <span class="split_content_label"><?php echo $label; ?></span>
@@ -17,14 +26,5 @@ $content = get_sub_field( 'content' );
         <?php echo $content; ?>
       </div>
     </div>
-    <?php
-      if ( have_rows( 'sidebar' ) ) :
-        while ( have_rows( 'sidebar' ) ) :
-          the_row();
-
-          get_template_part( 'layouts/fw-split_content_sidebar', get_row_layout() );
-        endwhile;
-      endif;
-    ?>
   </div>
 </div>
