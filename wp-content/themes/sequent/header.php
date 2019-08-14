e72fd87275388400bdab537c579b0322b732f3fe
Scott Revell <srevell@whitepenny.com>
2019-08-14 08:10:58 -0400

    Added Google Tag Manager

--- wp-content/themes/sequent/header.php
+++ wp-content/themes/sequent/header.php
@@ -26,12 +26,25 @@ $scripts_body = get_field( 'scripts_body', 'option' );
       gtag('config', 'UA-53107265-1');
     </script>
 
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PCTC43F');</script>
+<!-- End Google Tag Manager -->
+
     
     <?php wp_head(); ?>
     <?php sl_favicons(); ?>
     <?php echo $scripts_head; ?>
   </head>
-  <body <?php body_class( 'fs-grid fs-grid-fluid' ); ?> >
+  <body <?php body_class( 'fs-grid fs-grid-fluid' ); ?>>
+    
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PCTC43F"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <?php echo $scripts_body; ?>
 
     <div class="container js-mobile_nav_content">
