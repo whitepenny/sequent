420211b25cc6c7db5d433b1fa94380b458717d41
Scott Revell <srevell@whitepenny.com>
2019-11-15 09:20:47 -0500

    Add Button to Schedule on Homepage

--- wp-content/themes/sequent/gulpfile.js
+++ wp-content/themes/sequent/gulpfile.js
@@ -59,7 +59,7 @@ gulp.task('styles', function() {
       plugins: [ new lessImportNPM() ]
     }))
     .pipe(autoprefixer({
-      browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', 'ie >= 10']
+      overrideBrowserslist: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', 'ie >= 10']
     }))
     .pipe(cleanCSS({
       compatibility: 'ie10',
