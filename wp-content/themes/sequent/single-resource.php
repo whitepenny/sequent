95312e06c420e8769c57fa767252b4d7832826f4
Scott Revell <srevell@whitepenny.com>
2019-11-15 12:44:18 -0500

    Resources Sidebar

--- wp-content/themes/sequent/single-resource.php
+++ wp-content/themes/sequent/single-resource.php
@@ -18,15 +18,20 @@ if ( have_posts() ) :
       $meta_parts[] = '<span>By</span> ' . get_the_author_posts_link();
     }
 
-    sl_template_part( 'layouts/partial-page_header', array(
-      'page_class' => 'post_detail',
-      'page_title' => $post_title,
-    ) );
 ?>
-<div class="fs-row padded_row fs-all-justify-center page_main post_detail">
-  <div class="fs-cell fs-md-5 fs-lg-9 fs-xl-8 page_container">
+
+<div class="fs-row padded_row fs-all-justify-between page_main post_detail">
+  
+  <div class="fs-cell fs-lg-8">
+    <div class="resource_content">
+      <h1><?php echo $post_title; ?></h1>
+    </div>
+  </div>
+  <div class="fs-cell fs-md-5 fs-lg-8 page_container">
     <div class="page_content resource_content">
 
+
+      
       <?php the_content(); ?>
 
       <div class="post_social addthis_toolbox">
@@ -59,6 +64,37 @@ if ( have_posts() ) :
         </a>
       </div>
       <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid="></script>
+    </div>
+  </div>
+  <div class="fs-cell fs-lg-3">
+    <div class="resource-sidebar">
+    <h2>More Resources You Might Like</h2>
+    <?php 
+    $currentId = get_the_ID();
+    $args = array(
+      'post_type' => 'resource',
+      'posts_per_page' => 4,
+      'orderby' => 'menu_order',
+      'order' => 'ASC',
+      'post__not_in' => array($currentId),
+    );
+
+    $customQuery = new WP_Query( $args ); ?>
+
+
+
+    <?php if($customQuery->have_posts()) : ?>
+
+      <ul>
+      <?php while($customQuery->have_posts()) : $customQuery->the_post(); ?>
+
+      <li>
+        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+      </li>
+
+      <?php endwhile; ?>
+      </ul>
+    <?php endif; ?>
     </div>
   </div>
 </div>
